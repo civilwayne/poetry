9bec2c9027c7e35ef5a1453b70b2c3c3a03c664f
i7PC_wayne <civilwayne@gmail.com>
2013-09-14 00:26:39 +0800

    test github

---
+++ search.php
@@ -0,0 +1,3 @@
+<form method="post" action="../middleware/handler/search_handler.php">
+<label for="searchquery"><span class="caption">Search this site</span> <input type="text" size="20" maxlength="255" title="Enter your keywords and click the search button" name="searchquery" /></label> <input type="submit" value="Search" />
+</form>
